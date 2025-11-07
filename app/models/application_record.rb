3d557044022ae8759ab1cc2cf2372a1150f801b9
Igor Alexandre <igoralexandrevieira652@gmail.com>
2025-11-06 20:56:54 -0400

    traducao e busca configurados

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,11 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
   def self.ransackable_attributes(_auth_object = nil)
-    %w[]
+    %w[created_at created_by deleted_at updated_at updated_by descricao id nome]
   end
 
   def self.ransackable_associations(_auth_object = nil)
-    %w[]
+    %w[user]
   end
 
   def self.ransackable_associations(_auth_object = nil)
